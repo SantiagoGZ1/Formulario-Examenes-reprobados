4dfdc84e5454561353b44a00aab4578b3d4bf3bb
Santiago <santiagogarcia317109@correo.itm.edu.co>
2022-09-26 17:56:32 -0500

    Terminado: Se agregó un evento para al dar en check, aparezca un botó submit

--- curos.js
+++ curos.js
@@ -3,7 +3,7 @@ const name = document.querySelector('#name');
 const mail = document.querySelector('#mail');
 const subject1= document.querySelector('#subjects1');
 const subject2 = document.querySelector('#subjects2');
-
+const submit = document.querySelector('#containerSubmit');
 
 envio.addEventListener('click', (e)=>{
   let nameValue = console.log(e,name.value);
@@ -13,9 +13,14 @@ envio.addEventListener('click', (e)=>{
   if (name.value == ("Cofla") && e,mail.value == e,mail.value.includes ("@") && e,mail.value.includes (".com") ){
     alert("Bienvenido Cofla");
     if (subject1.value != subject2.value){
-      alert(`Se elegieron ${e,subject1.value} y ${e,subject2.value} con exito. \nDebe estar pendiente de su correo "${e,mail.value}" para recibir mas informacion`);
+      alert(`Se elegieron ${e,subject1.value} y ${e,subject2.value} con exito.`);
+      alert("Presione en el boton submit para enviar los datos.");
+      containerSubmit.innerHTML = `<button style="color:red" type="submit" id="submit">Submit</button>`;
+      submit.addEventListener('click', (e)=>{
+        alert(`Los datos fueron enviados con exito. \nLe llegara un mail a "${e,mail.value}" con la información.`);
+      });
     }else{
-      alert("No se puede elegir dos veces la misma materia \n Por favor elija otra materia");
+      alert("No se puede elegir dos veces la misma materia \nPor favor elija materias diferentes.");
     }
   }else{
     alert("Datos incorrectos");
